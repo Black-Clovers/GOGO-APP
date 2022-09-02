b2a234ad8c2624bc346999766c3d7d46f9474885
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-09-02 19:55:58 +0530

    Feature/dev (#11)
    * #commit : separate folders
    
    * Feature/Separate folders (#6)
    
    * Create readme file
    
    * Feat (Project) : init project
    
    * Feat(project) : setup folder structure (#2)
    
    * Update ReadeMe (#5)
    
    * update ReadeMe
    
    * Update ReadMe
    
    * #commit : separate folders
    
    Co-authored-by: Heshan Rathsara Kotuwe Gedara <88552872+heshanRkotuwegedara@users.noreply.github.com>
    
    * #commit : create folder structure
    
    * Feature/dev (#8)
    
    * Create readme file
    
    * Feat (Project) : init project
    
    * Feat(project) : setup folder structure (#2)
    
    * Update ReadeMe (#5)
    
    * update ReadeMe
    
    * Update ReadMe
    
    * #commit : separate folders
    
    * #commit : create folder structure
    
    Co-authored-by: Heshan Rathsara Kotuwe Gedara <88552872+heshanRkotuwegedara@users.noreply.github.com>
    
    * feat(Layout):Admin layout and client layout
    
    * feat(Route):create route file
    
    * Feature/it20614126 (#10)
    
    * Create readme file
    
    * Feat (Project) : init project
    
    * Feat(project) : setup folder structure (#2)
    
    * Update ReadeMe (#5)
    
    * update ReadeMe
    
    * Update ReadMe
    
    * Layouts and Routes (#9)
    
    * #commit : separate folders
    
    * #commit : create folder structure
    
    * feat(Layout):Admin layout and client layout
    
    * feat(Route):create route file
    
    Co-authored-by: Heshan Rathsara Kotuwe Gedara <88552872+heshanRkotuwegedara@users.noreply.github.com>
    
    * delete .gitkeep file
    
    * delete layout file
    
    * delete .gitkeep file
    
    * delete .gitkeep file
    
    * fix(route.js):rename
    
    * delete unwanted file
    
    Co-authored-by: Heshan Rathsara Kotuwe Gedara <88552872+heshanRkotuwegedara@users.noreply.github.com>

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const AppRoutes = () => {
+  return (
+    <div>
+      <Router>
+        <Routes></Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default AppRoutes;
