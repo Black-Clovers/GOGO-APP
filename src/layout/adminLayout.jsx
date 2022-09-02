c18d534222a23acee4fd62bd2837b4afd29600a7
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-09-02 19:49:06 +0530

    Layouts and Routes (#9)
    * #commit : separate folders
    
    * #commit : create folder structure
    
    * feat(Layout):Admin layout and client layout
    
    * feat(Route):create route file

---
+++ src/layout/adminLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminLayout = ({ children }) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+export default AdminLayout;
---
+++ src/layout/clientLayout.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../components/admin/common/header/Header';
+
+const ClientLayout = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default ClientLayout;
