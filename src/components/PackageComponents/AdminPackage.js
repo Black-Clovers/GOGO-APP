60ec7c6587452010cc702e44a99d58cbe64d9e06
dimanthathilakarathna98 <walahanduwathilakarathna@gmail.com>
2022-09-02 17:13:27 +0530

    create adminpackage frontend ,create routes,create package css

---
+++ src/components/PackageComponents/AdminPackage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function AdminPackage(){
+
+return (
+<div>
+
+
+    <h1>hello world spm</h1>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+);
+}
+
+export default AdminPackage;
+
+
