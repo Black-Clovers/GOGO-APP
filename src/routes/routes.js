4424b70bb68c6a40d74a72b17243fdd10c569de3
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-09-02 19:47:50 +0530

    feat(Route):create route file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,26 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Dashboard from "../pages/admin/dashboard/Dashboard";
-import Staff from "../pages/admin/staff/Staff";
-import Labs from "../pages/admin/lab/Lab";
-import Header from "../components/admin/common/admin/header/Header";
-import Sidebar from "../components/admin/common/admin/sidebar/Sidebar";
-import Treatment from "../pages/admin/treatment/Treatment";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const AppRoutes = () => {
-    return (
-        <div>
-            <Router>
-                <Header />
-                <Sidebar />
-                <Routes>
-                    <Route path="/" element={<Dashboard />} exact={true}/>
-                    <Route path="/staff" element={<Staff />} exact={true}/>
-                    <Route path="/labs" element={<Labs />} exact={true}/>
-                    <Route path="/treatment" element={<Treatment />} exact={true}/>
-                </Routes>
-            </Router>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Router>
+        <Routes></Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default AppRoutes;
