4dd88bc19f5e2c88b2193671622ab57b3649581b
Samitha Dilshan <it20607678@my.sliit.lk>
2022-09-05 20:58:44 +0530

    # commit : add vacancy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import AppRoutes from "./routes/AppRoutes";
 import AdminLayout from "./layout/adminLayout";
+import { Route } from 'react-router-dom';
 
 function App() {
     return (
         <AdminLayout class="wrapper">
             <AppRoutes/>
         </AdminLayout>
+
+       
     );
 }
 
