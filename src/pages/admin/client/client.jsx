337b170cce3c15b838f50f32bb0bfbc7f39f8971
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-09-07 22:18:17 +0530

    feat(Client): Profile picture function

--- src/pages/admin/client/client.jsx
+++ src/pages/admin/client/client.jsx
@@ -5,13 +5,28 @@ import axios from 'axios'
 
 const Client = () => {
     const [clients, setClients] = useState([]);
+    const [client_ID, setClient_ID] = useState("");
+    const [client_FirstName, setClient_FirstName] = useState("");
+    const [client_LastName, setClient_LastName] = useState("");
+    const [client_profilePicture, setClient_profilePicture] = useState("");
+    const [client_UserName, setClient_UserName] = useState("");
+    const [client_Email, setClient_Email] = useState("");
+    const [client_Mobile, setClient_Mobile] = useState("");
+    const [client_NIC, setClient_NIC] = useState("");
+    const [client_Password, setClient_Password] = useState("");
+    const [client_Gender, setClient_Gender] = useState("");
+    const [client_DOB, setClient_DOB] = useState("");
+    const [client_Status, setClient_Status] = useState("");
+    const [client_Address, setClient_Address] = useState("");
 
     useEffect(() => {
         getAll();
-    }, [])
+    }, []);
+
 
     const getAll = () => {
         axios.get("http://localhost:8000/api/client/all/").then((response) => {
+            console.log(response.data.data);
             setClients(response.data.data);
         })
     };
@@ -48,6 +63,57 @@ const Client = () => {
         });
     };
 
+    const addImageToProfile = () => {
+        let imgDiv = document.getElementById("imgInputDiv");
+
+        let imgUploader = document.createElement("input");
+        imgUploader.setAttribute("id", "imgUploader");
+        imgUploader.setAttribute("type", "file");
+        imgUploader.setAttribute("accept", "image/png, image/gif, image/jpeg");
+        imgUploader.setAttribute("class", "d-none")
+        imgDiv.appendChild(imgUploader);
+
+        let imgUploaderElement = document.getElementById("imgUploader");
+        console.log(imgUploaderElement);
+
+        if (imgUploaderElement !== undefined && imgUploaderElement !== null) {
+            imgUploaderElement.click();
+            imgUploaderElement.addEventListener("change", () => {
+                imgUploaderElement = document.getElementById("imgUploader");
+                console.log(imgUploaderElement);
+                if (imgUploaderElement.files[0] !== null && imgUploaderElement.files[0] !== undefined) {
+                    console.log("Hello")
+                    let fileReader = new FileReader();
+                    fileReader.onload = function (event) {
+
+                        console.log(event)
+                        setClient_profilePicture(event.target.result);
+                    }
+                }
+                // setClient_profilePicture(imgUploaderElement.file[0]);
+            });
+        }
+    }
+
+    const addClient = () => {
+        const newClient = {
+            "client_ID": client_ID,
+            "client_FirstName": client_FirstName,
+            "client_LastName": client_LastName,
+            "client_profilePicture": client_profilePicture,
+            "client_UserName": client_UserName,
+            "client_Email": client_Email,
+            "client_Mobile": client_Mobile,
+            "client_NIC": client_NIC,
+            "client_Password": client_Password,
+            "client_Gender": client_Gender,
+            "client_DOB": client_DOB,
+            "client_Status": client_Status,
+            "client_Address": client_Address,
+        }
+
+        console.log(newClient);
+    }
 
     return (
         <div>
@@ -70,7 +136,9 @@ const Client = () => {
                                     <div className="d-flex justify-content-end align-items-center">
                                         <div className="d-flex justify-content-center align-items-center">
                                             <input id="searchID" type="text" className="form-control col-8 me-5"
-                                                   placeholder="ID"/>
+                                                   placeholder="ID" onChange={(e) => {
+                                                setClient_ID(e.target.value)
+                                            }}/>
                                         </div>
                                         <div>
                                             <input type="button" className="form-control btnSearch text-white"
@@ -87,7 +155,9 @@ const Client = () => {
                                 <div className="col d-flex justify-content-end align-items-center">
                                     <div className="col d-flex justify-content-end">
                                         <div>
-                                            <button className="btn btnAddImg" type="button">
+                                            <button className="btn btnAddImg" type="button" onClick={() => {
+                                                addImageToProfile()
+                                            }}>
                                                 <i className="fa fa-plus text-white" aria-hidden="true"/>
                                             </button>
                                             <button className="btn btnEditImg" type="button">
@@ -98,9 +168,9 @@ const Client = () => {
                                             </button>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="imgDiv">
-
+                                    <div id="imgInputDiv">
+                                        <div>
+                                            <img className="imgDiv" src={client_profilePicture} alt=""/>
                                         </div>
                                     </div>
                                 </div>
@@ -108,34 +178,58 @@ const Client = () => {
 
                             <div className="row mt-4">
                                 <div className="col">
-                                    <input type="text" className="form-control" placeholder="First Name"/>
+                                    <input type="text" className="form-control" placeholder="First Name"
+                                           onChange={(e) => {
+                                               setClient_FirstName(e.target.value)
+                                           }}/>
                                 </div>
                                 <div className="col">
-                                    <input type="text" className="form-control" placeholder="Last Name"/>
-                                </div>
-                            </div>
-                            <div className="row mt-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="User Name"/>
-                                </div>
-                                <div className="col">
-                                    <input type="email" className="form-control" placeholder="Email"/>
+                                    <input type="text" className="form-control" placeholder="Last Name"
+                                           onChange={(e) => {
+                                               setClient_LastName(e.target.value)
+                                           }}/>
                                 </div>
                             </div>
                             <div className="row mt-4">
                                 <div className="col">
-                                    <input type="tel" className="form-control" placeholder="Mobile Number"/>
+                                    <input type="text" className="form-control" placeholder="User Name"
+                                           onChange={(e) => {
+                                               setClient_UserName(e.target.value)
+                                           }}
+                                    />
                                 </div>
                                 <div className="col">
-                                    <input type="text" className="form-control" placeholder="NIC"/>
+                                    <input type="email" className="form-control" placeholder="Email"
+                                           onChange={(e) => {
+                                               setClient_Email(e.target.value)
+                                           }}
+                                    />
                                 </div>
                             </div>
                             <div className="row mt-4">
                                 <div className="col">
-                                    <select name="gender" className="form-select" aria-label="role">
-                                        {/*<option selected={true} disabled defaultValue="0">Gender</option>*/}
-                                        {/*<option defaultValue="1">Male</option>*/}
-                                        {/*<option defaultValue="2">Female</option>*/}
+                                    <input type="tel" className="form-control" placeholder="Mobile Number"
+                                           onChange={(e) => {
+                                               setClient_Mobile(e.target.value)
+                                           }}
+                                    />
+                                </div>
+                                <div className="col">
+                                    <input type="text" className="form-control" placeholder="NIC"
+                                           onChange={(e) => {
+                                               setClient_NIC(e.target.value)
+                                           }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row mt-4">
+                                <div className="col">
+                                    <select name="gender" className="form-select" aria-label="role" onChange={(e) => {
+                                        setClient_Gender(e.target.value)
+                                    }}>
+                                        <option selected disabled value="0">Gender</option>
+                                        <option defaultValue="1">Male</option>
+                                        <option defaultValue="2">Female</option>
                                     </select>
                                 </div>
                                 <div className="col">
@@ -143,19 +237,26 @@ const Client = () => {
                                            className="form-control"
                                            placeholder="Date of Birth"
                                            type="text"
-                                           onFocus={(e) => e.target.type = 'date'} id="dateOfBirth"/>
+                                           onFocus={(e) => e.target.type = 'date'} id="dateOfBirth" onChange={(e) => {
+                                        setClient_DOB(e.target.value)
+                                    }}/>
                                 </div>
                                 <div className="col">
-                                    {/*<select name="status" className="form-select" aria-label="role">*/}
-                                    {/*    <option selected disabled value="0">Status</option>*/}
-                                    {/*    <option value="1">Online</option>*/}
-                                    {/*    <option value="2">Offline</option>*/}
-                                    {/*</select>*/}
+                                    <select name="status" className="form-select" aria-label="role" onChange={(e) => {
+                                        setClient_Status(e.target.value)
+                                    }}>
+                                        <option selected disabled value="0">Status</option>
+                                        <option value="1">Online</option>
+                                        <option value="2">Offline</option>
+                                    </select>
                                 </div>
                             </div>
                             <div className="row mt-5">
                                 <div className="d-flex justify-content-around align-items-center">
-                                    <button type="submit" className="btn btnRegister ">Register</button>
+                                    <button type="button" className="btn btnRegister" onClick={() => {
+                                        addClient()
+                                    }}>Register
+                                    </button>
                                     <button type="button" className="btn btnUpdate">Update</button>
                                     <button type="button" className="btn btnDelete">Delete</button>
                                 </div>
