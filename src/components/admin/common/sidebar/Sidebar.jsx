3b62b0516a07fb6ce9c164d10008f1655d10721f
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-09-10 15:37:26 +0530

    chore(SideBar):  fix new routes

--- src/components/admin/common/sidebar/Sidebar.jsx
+++ src/components/admin/common/sidebar/Sidebar.jsx
@@ -5,31 +5,31 @@ import {Link} from "react-router-dom";
 const Sidebar = () => {
     return (<div className="sidebar">
         <ul>
-            <li><Link to="/">
+            <li><Link to="/admin/">
                 <span className="icon"><i className="fa fa-tachometer"/></span>
                 <span className="title">Dashboard</span>
             </Link></li>
-            <li><Link to="/driver">
+            <li><Link to="/admin/driver">
                 <span className="icon"><i className="fa-solid fa-user-group"/></span>
                 <span className="title">Driver</span>
             </Link></li>
-            <li><Link to="/client">
+            <li><Link to="/admin/client">
                 <span className="icon"><i className="fa fa-female" aria-hidden="true"/></span>
                 <span className="title">Client</span>
             </Link></li>
-            <li><Link to="/vehicle">
+            <li><Link to="/admin/vehicle">
                 <span className="icon"><i className="fa fa-car" aria-hidden="true"/></span>
                 <span className="title">Vehicle</span>
             </Link></li>
-            <li><Link to="/package">
+            <li><Link to="/admin/package">
                 <span className="icon"><i className="fa fa-archive" aria-hidden="true"/></span>
                 <span className="title">Package</span>
             </Link></li>
-            <li><Link to="/ride">
+            <li><Link to="/admin/ride">
                 <span className="icon"><i className="fa fa-location-arrow" aria-hidden="true"/></span>
                 <span className="title">Ride</span>
             </Link></li>
-            <li><Link to="/careers">
+            <li><Link to="/admin/careers">
                 <span className="icon"><i className="fa fa-user-tie" aria-hidden="true"/></span>
                 <span className="title">Career</span>
             </Link></li>
