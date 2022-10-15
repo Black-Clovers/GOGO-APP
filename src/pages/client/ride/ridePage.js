23db85282034258e8c6c64b6c2983e657f12c2cb
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-10-15 16:58:48 +0530

    #Client side Ride page add function done

--- src/pages/client/ride/ridePage.js
+++ src/pages/client/ride/ridePage.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import '../ride/ride.css';
 import axios from 'axios';
 import MapContainer from "../../../components/admin/common/map/MapContainer";
+import VueSweetalert2 from "sweetalert2";
 
 function RidePage(props) {
 
@@ -9,20 +10,15 @@ function RidePage(props) {
         return <h1>{status}</h1>;
     };
 
-    const [clients, setClients] = useState([]);
-    const [client_ID, setClient_ID] = useState("");
-    const [client_FirstName, setClient_FirstName] = useState("");
-    const [client_LastName, setClient_LastName] = useState("");
-    const [client_profilePicture, setClient_profilePicture] = useState("");
-    const [client_UserName, setClient_UserName] = useState("");
-    const [client_Email, setClient_Email] = useState("");
-    const [client_Mobile, setClient_Mobile] = useState("");
-    const [client_NIC, setClient_NIC] = useState("");
-    const [client_Password, setClient_Password] = useState("12345");
-    const [client_Gender, setClient_Gender] = useState("");
-    const [client_DOB, setClient_DOB] = useState("");
-    const [client_Status, setClient_Status] = useState("");
-    const [client_Address, setClient_Address] = useState("");
+    const [trips, setTrips] = useState([]);
+    const [pickUpLocation, setPickUpLocation] = useState('');
+    const [dropOffLocation, setDropOffLocation] = useState('');
+    const [tripType, setTripType] = useState("OneWay");
+    const [pickUpdate, setPickUpdate] = useState("15-10-2022");
+    const [pickUpTime, setPickUpTime] = useState("15.25 P.M");
+    const [vehicleType, setVehicleType] = useState("Car");
+    const [currentUser, setCurrentUser] = useState({});
+    const [currentDriver, setCurrentDriver] = useState({});
 
 
     useEffect(() => {
@@ -31,139 +27,40 @@ function RidePage(props) {
 
 
     const getAll = () => {
-        axios.get("http://localhost:8000/api/client/all/").then((response) => {
+        axios.get("http://localhost:8000/api/trip/all/").then((response) => {
             console.log(response.data.data);
-            setClients(response.data.data);
+            setTrips(response.data.data);
         })
     };
 
-    const editClient = (client) => {
-        console.log("Hello")
-        setClient_ID(client._id)
-        setClient_profilePicture(client.client_profilePicture);
-        setClient_FirstName(client.client_FirstName);
-        setClient_LastName(client.client_LastName);
-        setClient_UserName(client.client_UserName);
-        setClient_Email(client.client_Email);
-        setClient_Mobile(client.client_Mobile);
-        setClient_NIC(client.client_NIC);
-        setClient_Address(client.client_Address);
-        setClient_Gender(client.client_Gender);
-        setClient_DOB(client.client_DOB);
-        setClient_Status(client.client_Status);
-
-        document.getElementById("btnImgDelete").removeAttribute("disabled");
-        document.getElementById("btnEditImg").removeAttribute("disabled");
-        document.getElementById("btnAddImg").setAttribute("disabled", "true");
-    }
-
-    const displayAllClients = () => {
-        return clients.map((client) => {
-            return (<tr itemScope="row" id={client._id} key={client._id}>
-                <td>
-                    {client._id}
-                </td>
-                <td>{client.client_FirstName}</td>
-                <td>
-                    {client.client_LastName}
-                </td>
-                <td>
-                    <img src={client.client_profilePicture} alt="profile picture" width={25} height={25}/>
-                </td>
-                <td> {client.client_UserName}</td>
-                <td> {client.client_Address}</td>
-                <td> {client.client_Mobile}</td>
-                <td> {client.client_Email}</td>
-                <td> {client.client_Gender}</td>
-                <td> {client.client_NIC}</td>
-                {/*<td> {client.client_Password}</td>*/}
-
-                <td> {client.client_DOB}</td>
-                <td> {client.client_Status}</td>
-
-                <td>
-                    <i className="fa-solid fa-pen me-3 text-primary d-inline" onClick={() => {
-                        editClient(client)
-                    }}/>
-                    <i className="fa-solid fa-trash-can d-inline me-2 text-danger d-inline" onClick={() => {
-                        deleteClient(client)
-                    }}/>
-                </td>
-            </tr>)
-        });
-    };
-
-    const addClient = () => {
-        const newClient = {
-            "client_ID": "client_ID",
-            "client_FirstName": client_FirstName,
-            "client_LastName": client_LastName,
-            "client_profilePicture": client_profilePicture,
-            "client_UserName": client_UserName,
-            "client_Email": client_Email,
-            "client_Mobile": client_Mobile,
-            "client_NIC": client_NIC,
-            "client_Password": client_Password,
-            "client_Gender": client_Gender,
-            "client_DOB": client_DOB,
-            "client_Status": client_Status,
-            "client_Address": client_Address,
+    const addTrip = () => {
+        const newTrip = {
+            "trip_ID": "trip_ID",
+            "trip_Type": tripType,
+            "client": currentUser,
+            "driver": currentDriver,
+            "trip_pickUp_Location": {},
+            "trip_dropOff_Location": {},
+            "trip_pickUp_Date": pickUpdate,
+            "trip_pickUp_Time": pickUpTime,
+            "trip_vehicle_Type": vehicleType,
+            "trip_Status": "0",
         }
 
-        axios.post("http://localhost:8000/api/client/", newClient).then((response) => {
+        axios.post("http://localhost:8000/api/trip/", newTrip).then((response) => {
             if (response.data.result.response) {
-                alert("RidePage Added");
+                VueSweetalert2.fire({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    icon: 'success',
+                    title: 'RidePage Added',
+                });
                 getAll();
             }
         })
     }
-
-    const updateClient = () => {
-        const newClient = {
-            "client_ID": client_ID,
-            "client_FirstName": client_FirstName,
-            "client_LastName": client_LastName,
-            "client_profilePicture": client_profilePicture,
-            "client_UserName": client_UserName,
-            "client_Email": client_Email,
-            "client_Mobile": client_Mobile,
-            "client_NIC": client_NIC,
-            "client_Password": client_Password,
-            "client_Gender": client_Gender,
-            "client_DOB": client_DOB,
-            "client_Status": client_Status,
-            "client_Address": client_Address,
-        }
-
-        axios.put("http://localhost:8000/api/client/", newClient).then((response) => {
-            if (response.data.result.response) {
-                alert("RidePage Updated");
-                getAll();
-            }
-        })
-    }
-
-    const deleteClient = (client) => {
-        console.log(client)
-        axios.delete(`http://localhost:8000/api/client/${client._id}`).then((response) => {
-            if (response.data.result.response) {
-                alert("RidePage Deleted");
-                getAll();
-            }
-        })
-    }
-
-    const searchClient = () => {
-        if (client_ID === null || client_ID === undefined || client_ID === "") {
-            alert("Please insert the client ID");
-        } else {
-            axios.get(`http://localhost:8000/api/client/${client_ID}`).then((response) => {
-                let searchedClient = [];
-                searchedClient.push(response.data.data)
-                setClients(searchedClient);
-            })
-        }
-    };
 
     return (
         <div className="RideDiv px-4">
@@ -174,11 +71,15 @@ function RidePage(props) {
                             <div className="row mt-4">
                                 <div className="col">
                                     <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" id="oneWay"/>
+                                        <input className="form-check-input" type="radio" id="oneWay" onChange={(e) => {
+                                            setTripType(e.target.value)
+                                        }} value={tripType}/>
                                         <label className="form-check-label" htmlFor="oneWay">One Way Trip</label>
                                     </div>
                                     <div className="form-check form-check-inline">
-                                        <input className="form-check-input" type="radio" id="return"/>
+                                        <input className="form-check-input" type="radio" id="return" onChange={(e) => {
+                                            setTripType(e.target.value)
+                                        }} value={tripType}/>
                                         <label className="form-check-label" htmlFor="return">Return Trip</label>
                                     </div>
                                 </div>
@@ -187,17 +88,17 @@ function RidePage(props) {
                                 <div className="col">
                                     <input type="text" className="form-control" placeholder="Pickup Location"
                                            onChange={(e) => {
-                                               setClient_UserName(e.target.value)
+                                               setPickUpLocation(e.target.value)
                                            }}
-                                           value={client_UserName}
+                                           value={pickUpLocation}
                                     />
                                 </div>
                                 <div className="col">
-                                    <input type="email" className="form-control" placeholder="Drop Off Location"
+                                    <input type="text" className="form-control" placeholder="Drop Off Location"
                                            onChange={(e) => {
-                                               setClient_Email(e.target.value)
+                                               setDropOffLocation(e.target.value)
                                            }}
-                                           value={client_Email}
+                                           value={dropOffLocation}
                                     />
                                 </div>
                             </div>
@@ -212,9 +113,9 @@ function RidePage(props) {
                                            className="form-control"
                                            placeholder="Pick Up Date"
                                            type="text"
-                                           value={client_DOB}
+                                           value={pickUpdate}
                                            onFocus={(e) => e.target.type = 'date'} id="pickUpDate" onChange={(e) => {
-                                        setClient_DOB(e.target.value)
+                                        setPickUpdate(e.target.value)
                                     }}/>
                                 </div>
                                 <div className="col">
@@ -222,17 +123,17 @@ function RidePage(props) {
                                            className="form-control"
                                            placeholder="Pick Up Time"
                                            type="text"
-                                           value={client_DOB}
-                                           onFocus={(e) => e.target.type = 'date'} id="pickUpTime" onChange={(e) => {
-                                        setClient_DOB(e.target.value)
+                                           value={pickUpTime}
+                                           onFocus={(e) => e.target.type = 'time'} id="pickUpTime" onChange={(e) => {
+                                        setPickUpTime(e.target.value)
                                     }}/>
                                 </div>
                             </div>
                             <div className="row mt-4">
                                 <div className="col-6">
-                                    <select name="type" value={client_Gender} className="form-select"
+                                    <select name="type" value={vehicleType} className="form-select"
                                             aria-label="role" onChange={(e) => {
-                                        setClient_Gender(e.target.value)
+                                        setVehicleType(e.target.value)
                                     }}>
                                         <option selected disabled value="0">Vehicle Type</option>
                                         <option defaultValue="1">Car</option>
@@ -244,10 +145,10 @@ function RidePage(props) {
                             <div className="row mt-5 pb-4">
                                 <div className="d-flex justify-content-around align-items-center">
                                     <button type="button" className="btn btnRegister" onClick={() => {
-                                        addClient()
+                                        addTrip()
                                     }}>Register
                                     </button>
-                                    <button type="button" className="btn btnDelete">Cancel</button>
+                                    <button type="submit" className="btn btnDelete">Cancel</button>
                                 </div>
                             </div>
                         </form>
