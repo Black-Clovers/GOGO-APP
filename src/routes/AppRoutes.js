077411c4f1f5dd4c022dea855c2570d4e7f82971
Samitha Dilshan <it20607678@my.sliit.lk>
2022-10-21 19:16:42 +0530

    add registerVacancy

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,6 +4,7 @@ import Dashboard from '../pages/admin/dashboard/dashboard'
 import Package from '../pages/admin/travelpackage/TravelPackage'
 import Client from '../pages/admin/client/client'
 import Ride from '../pages/admin/ride/ride'
+import Career from '../pages/admin/CareerAdmin/addVacancy'
 import SignUp from "../pages/client/signup/SignUp";
 import ClientLayout from "../layout/clientLayout";
 import AdminLayout from "../layout/adminLayout";
@@ -12,6 +13,9 @@ import Sidebar from "../components/admin/common/sidebar/Sidebar";
 import ClientHeader from "../components/client/common/header/ClientHeader";
 import HomePage from "../pages/client/home/homePage";
 import RidePage from "../pages/client/ride/ridePage";
+import CareerList from "../pages/admin/CareerAdmin/CareerList";
+import CareerHome from "../pages/client/career/careerHome";
+import RegisterVacancy from "../pages/client/career/registerVacancy";
 
 
 const AppRoutes = () => {
@@ -28,6 +32,8 @@ const AppRoutes = () => {
                                     <Switch>
                                         <Route path="/client" render={(props) => <HomePage/>} exact/>;
                                         <Route path="/client/ride" render={(props) => <RidePage/>} />;
+                                        <Route path="/client/career" render={(props) => <CareerHome/>}/>;
+                                      <Route path="/client/registerVacancy" render={(props) => <RegisterVacancy/>}/>;
                                     </Switch>
                                 </main>
                             </ClientLayout>
@@ -43,6 +49,10 @@ const AppRoutes = () => {
                                 <Route path="/admin/package" render={(props) => <Package/>}/>;
                                 <Route path="/admin/client" render={(props) => <Client/>}/>;
                                 <Route path="/admin/ride" render={(props) => <Ride/>}/>;
+                                <Route path="/admin/career" render={(props) => <Career/>}/>;
+                                <Route path="/admin/careerlist" render={(props) => <CareerList/>}/>;
+                                
+
                             </Switch>
                         </AdminLayout>
                     </Route>
